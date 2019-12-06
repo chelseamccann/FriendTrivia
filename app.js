2fc6dfb3409f45de319155f656e21e2121da73a6
chels <chemccann@hotmail.com>
2019-12-06 17:56:24 -0500

    Updated with stylesheet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.use("/styles",express.static(__dirname + "/styles")); // allows stylesheet
 app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res, next){
     res.sendFile(__dirname + '/index.html');
@@ -10,7 +11,7 @@ app.get('/', function(req, res, next){
 
 io.on('connection', function(client){ // listening for connections
     console.log('Client connected...'); // once a connection is received, it reports this message to the console
-    
+
     client.on('join', function(data){ // waits for a message from the client for 'join' - then log it to the console
         console.log(data);
         // client.emit('messages', 'Hello from server'); // sends a message back to the client that just connect
