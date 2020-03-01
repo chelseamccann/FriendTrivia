a5bbbcc8099cf62b4a1c186cda45de96bdac4d78
chels <chemccann@hotmail.com>
2020-03-01 02:38:04 -0500

    Removed unnecesary code

--- javascript/game.js
+++ javascript/game.js
@@ -101,35 +101,4 @@ choices.forEach(choice => {
     });
 });
 
-var modal = function(){
-
-    var modal = document.getElementById("simple-modal");
-    var closeBtn = document.getElementsByClassName("close-btn")[0];
-
-    closeBtn.addEventListener("click", () => {
-        d3.select("#modal-school").node().remove()
-        while(d3.select(".modal-body-p").node()){
-            d3.select(".modal-body-p").node().remove()
-            d3.select("#wordcloud").node().remove()
-            // d3.select("#bob").node().remove()
-            d3.select("#pp").node().remove()
-        }
-        modal.style.display = "none";
-    });
-
-    window.addEventListener("click", (e) => {
-        if(e.target === modal){
-            d3.select("#modal-school").node().remove()
-            while(d3.select(".modal-body-p").node()){
-                d3.select(".modal-body-p").node().remove()
-                d3.select("#wordcloud").node().remove()
-                // d3.select("#bob").node().remove()
-                d3.select("#pp").node().remove()
-            }
-            modal.style.display = 'none';
-        }
-    })
-
-}
-
 startGame();
